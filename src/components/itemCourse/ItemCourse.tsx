2473b2f8e45e612c962c9aa519ac077be8a39dbd
keongy <info.pichon@gmail.com>
2022-04-20 18:24:25 +0200

    commit

--- src/components/itemCourse/ItemCourse.tsx
+++ src/components/itemCourse/ItemCourse.tsx
@@ -6,7 +6,7 @@ const ItemCourse: React.FC<Item> = (props) => {
 
     return (
         <div className='d-flex align-items-center justify-content-center'>
-            <h1 className='me-5'>item</h1>
+            <h1 className='me-5'>items</h1>
             <input type="checkbox" className='form-check-input' />
         </div>
     );
