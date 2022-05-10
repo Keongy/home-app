b59e8e585c041dd867cfbeb86b422ea96cd66c3f
keongy <info.pichon@gmail.com>
2022-05-10 12:58:40 +0200

    init db from authroute works perfect

--- src/App.tsx
+++ src/App.tsx
@@ -4,47 +4,12 @@ import { config } from './config/config';
 import Menu from './components/menu/Menu';
 import routes from './config/routes'
 import AuthRoute from './components/authRoute/AuthRoute';
-import { useEffect } from 'react';
-import { child, get, getDatabase, ref } from 'firebase/database';
-import { useDispatch } from 'react-redux';
-import { initList } from './redux/listCourse.reducer';
-import { initState } from './redux/course.reducer';
 
 
 initializeApp(config.firebaseConfig);
 
 
 const App = () => {
-  const dbRef = ref(getDatabase());
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    get(child(dbRef, 'homeApp/initList/list')).then((snapshot) => {
-      if (snapshot.exists()) {
-        // console.log('LIST', snapshot.val());
-        console.log('Connection DBREF')
-        dispatch(initList(snapshot.val()))
-      } else {
-        console.log("No data available");
-      }
-    }).catch((error) => {
-      console.error(error);
-    });
-  }, [])
-
-  get(child(dbRef, 'homeApp/listeCourse/list')).then((snapshot) => {
-    if (snapshot.exists()) {
-      // console.log('DATA', snapshot.val());
-      console.log('Connection DBREF2222')
-      dispatch(initState(snapshot.val()))
-    } else {
-      console.log("No data available");
-    }
-  }).catch((error) => {
-    console.error(error);
-  });
-
-
 
   return (
     <div className='vh-100'>
--- src/components/authRoute/AuthRoute.tsx
+++ src/components/authRoute/AuthRoute.tsx
@@ -1,8 +1,11 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import Login from "../../pages/login/Login";
 import Loading from "../../pages/loading/Loading";
+import { child, get, getDatabase, ref } from "firebase/database";
+import { useDispatch } from "react-redux";
+import { initList } from "../../redux/listCourse.reducer";
+import { initState } from "../../redux/course.reducer";
 
 export interface IAuthRouteProps {
     children: any
@@ -13,22 +16,63 @@ const AuthRoute: React.FunctionComponent<IAuthRouteProps> = (props) => {
     const auth = getAuth();
     const navigate = useNavigate();
     const [loading, setLoading] = useState<boolean>(true);
+    const [user, setUser] = useState<boolean>(false)
+
+    const dbRef = ref(getDatabase());
+    const dispatch = useDispatch()
+
+
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
             if (user) {
+                setUser(true)
                 setLoading(false);
                 console.log("There is an user !");
+
             } else {
+                setLoading(false);
                 console.log('unauthorized');
                 navigate('/login');
             }
         });
     }, [auth, navigate]);
 
+    useEffect(() => {
+        if (user) {
+            get(child(dbRef, 'homeApp/initList/list')).then((snapshot) => {
+                console.log("Step: APP UseEffect")
+                if (snapshot.exists()) {
+                    // console.log('LIST', snapshot.val());
+                    console.log('Connection DBREF')
+                    dispatch(initList(snapshot.val()))
+                } else {
+                    console.log("No data available");
+                }
+            }).catch((error) => {
+                console.error(error);
+            });
+
+            get(child(dbRef, 'homeApp/listeCourse/list')).then((snapshot) => {
+                console.log("Step: APP GET")
+                if (snapshot.exists()) {
+                    // console.log('DATA', snapshot.val());
+                    console.log('Connection DBREF2222')
+                    dispatch(initState(snapshot.val()))
+                } else {
+                    console.log("No data available");
+                }
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
+
+    }, [user])
+
+
     return loading ?
         (
-            <Login />
+            <Loading />
         )
         :
         (
